f30d678bb2835708718f7f9ff0a69e90e56e32db
ilyabelow <ilyabelow.almost.surely@gmail.com>
2023-05-02 11:22:06 +0300

    return of bitstream

---
+++ w7/bitstream.h
@@ -0,0 +1,101 @@
+#pragma once
+#include <cstring>
+
+// ===== data streaming
+
+template<class T>
+struct Skip{ };
+
+struct bitstream {
+  explicit bitstream(uint8_t *data): m_data(data) { }
+
+  template<class T>
+  friend bitstream& operator<<(bitstream&, const T&);
+  template<class T>
+  friend bitstream& operator<<(bitstream&&, const T&);
+
+  template<class T>
+  friend bitstream& operator>>(bitstream&, T&);
+  template<class T>
+  friend bitstream& operator>>(bitstream&&, T&);
+  template<class T>
+  friend bitstream& operator>>(bitstream&, Skip<T>&&);
+  template<class T>
+  friend bitstream& operator>>(bitstream&&, Skip<T>&&);
+
+
+private:
+  uint8_t *m_data;
+  size_t m_offset = 0;
+};
+
+// write
+
+template<class T>
+bitstream& operator<<(bitstream&& s, const T& t) { // r-value variant for in-place/implicit construction
+  memcpy(s.m_data + s.m_offset, &t, sizeof(T));
+  s.m_offset += sizeof(T);
+  return s;
+}
+
+template<class T>
+bitstream& operator<<(bitstream& s, const T& t) {
+  memcpy(s.m_data + s.m_offset, &t, sizeof(T));
+  s.m_offset += sizeof(T);
+  return s;
+}
+
+// read
+
+template<class T>
+bitstream& operator>>(bitstream& s, T& t) {
+  memcpy(&t, s.m_data + s.m_offset, sizeof(T));
+  s.m_offset += sizeof(T);
+  return s;
+}
+
+template<class T>
+bitstream& operator>>(bitstream&& s, T& t) { // r-value variant for in-place/implicit construction
+  memcpy(&t, s.m_data + s.m_offset, sizeof(T));
+  s.m_offset += sizeof(T);
+  return s;
+}
+
+template<class T>
+bitstream& operator>>(bitstream& s, Skip<T>&&) {
+  s.m_offset += sizeof(T);
+  return s;
+}
+
+template<class T>
+bitstream& operator>>(bitstream&& s, Skip<T>&&) {
+  s.m_offset += sizeof(T);
+  return s;
+}
+
+// === size measurement
+
+struct ruler {
+  operator size_t() {
+    return m_size;
+  }
+
+  template<class T>
+  friend ruler& operator<<(ruler&, const T&);
+  template<class T>
+  friend ruler& operator<<(ruler&&, const T&);
+private:
+  size_t m_size;
+};
+
+template<class T>
+ruler& operator<<(ruler& r, const T&) {
+  r.m_size += sizeof(T);
+  return r;
+}
+
+template<class T>
+ruler& operator<<(ruler&& r, const T&) { // r-value variant for in-place/implicit construction
+  r.m_size += sizeof(T);
+  return r;
+}
--- w7/protocol.cpp
+++ w7/protocol.cpp
@@ -2,6 +2,7 @@
 #include "quantisation.h"
 #include <cstring> // memcpy
 #include <iostream>
+#include "bitstream.h"
 
 void send_join(ENetPeer *peer)
 {
@@ -13,22 +14,18 @@ void send_join(ENetPeer *peer)
 
 void send_new_entity(ENetPeer *peer, const Entity &ent)
 {
-  ENetPacket *packet = enet_packet_create(nullptr, sizeof(uint8_t) + sizeof(Entity),
-                                                   ENET_PACKET_FLAG_RELIABLE);
-  uint8_t *ptr = packet->data;
-  *ptr = E_SERVER_TO_CLIENT_NEW_ENTITY; ptr += sizeof(uint8_t);
-  memcpy(ptr, &ent, sizeof(Entity)); ptr += sizeof(Entity);
+  size_t size = ruler() << E_SERVER_TO_CLIENT_NEW_ENTITY << ent;
+  ENetPacket *packet = enet_packet_create(nullptr, size, ENET_PACKET_FLAG_RELIABLE);
+  bitstream(packet->data) << E_SERVER_TO_CLIENT_NEW_ENTITY << ent;
 
   enet_peer_send(peer, 0, packet);
 }
 
 void send_set_controlled_entity(ENetPeer *peer, uint16_t eid)
 {
-  ENetPacket *packet = enet_packet_create(nullptr, sizeof(uint8_t) + sizeof(uint16_t),
-                                                   ENET_PACKET_FLAG_RELIABLE);
-  uint8_t *ptr = packet->data;
-  *ptr = E_SERVER_TO_CLIENT_SET_CONTROLLED_ENTITY; ptr += sizeof(uint8_t);
-  memcpy(ptr, &eid, sizeof(uint16_t)); ptr += sizeof(uint16_t);
+  size_t size = ruler() << E_SERVER_TO_CLIENT_SET_CONTROLLED_ENTITY << eid;
+  ENetPacket *packet = enet_packet_create(nullptr, size, ENET_PACKET_FLAG_RELIABLE);
+  bitstream(packet->data) << E_SERVER_TO_CLIENT_SET_CONTROLLED_ENTITY << eid;
 
   enet_peer_send(peer, 0, packet);
 }
@@ -37,19 +34,11 @@ typedef PackedVec2<uint8_t, 4, 4> ControlsQuantized;
 
 void send_entity_input(ENetPeer *peer, uint16_t eid, float thr, float ori)
 {
-  ENetPacket *packet = enet_packet_create(nullptr, sizeof(uint8_t) + sizeof(uint16_t) +
-                                                   sizeof(ControlsQuantized),
-                                                   ENET_PACKET_FLAG_UNSEQUENCED);
-  uint8_t *ptr = packet->data;
-  *ptr = E_CLIENT_TO_SERVER_INPUT; ptr += sizeof(uint8_t);
-  memcpy(ptr, &eid, sizeof(uint16_t)); ptr += sizeof(uint16_t);
-
   ControlsQuantized controlsPacked({thr, ori}, {-1., 1.}, {-1., 1.});
-  memcpy(ptr, &controlsPacked.packedVal, sizeof(ControlsQuantized)); ptr += sizeof(ControlsQuantized);
-  /*
-  memcpy(ptr, &thrPacked, sizeof(uint8_t)); ptr += sizeof(uint8_t);
-  memcpy(ptr, &oriPacked, sizeof(uint8_t)); ptr += sizeof(uint8_t);
-  */
+
+  size_t size = ruler() << E_CLIENT_TO_SERVER_INPUT << eid << controlsPacked;
+  ENetPacket *packet = enet_packet_create(nullptr, size, ENET_PACKET_FLAG_UNSEQUENCED);
+  bitstream(packet->data) << E_CLIENT_TO_SERVER_INPUT << eid << controlsPacked;
 
   enet_peer_send(peer, 1, packet);
 }
@@ -58,16 +47,12 @@ typedef PackedVec3<uint32_t, 12, 11, 9> TransformQuantized;
 
 void send_snapshot(ENetPeer *peer, uint16_t eid, float x, float y, float ori)
 {
-  ENetPacket *packet = enet_packet_create(nullptr, sizeof(uint8_t) + sizeof(uint16_t) +
-                                                   sizeof(TransformQuantized),
-                                                   ENET_PACKET_FLAG_UNSEQUENCED);
-  uint8_t *ptr = packet->data;
-  *ptr = E_SERVER_TO_CLIENT_SNAPSHOT; ptr += sizeof(uint8_t);
-  memcpy(ptr, &eid, sizeof(uint16_t)); ptr += sizeof(uint16_t);
-
   TransformQuantized tmPacked({x, y, ori}, {-16., 16.}, {-8., 8.},  {-PI, PI});
-  //printf("xPacked/unpacked %d %f\n", xPacked, x);
-  memcpy(ptr, &tmPacked.packedVal, sizeof(TransformQuantized)); ptr += sizeof(TransformQuantized);
+
+  size_t size = ruler() << E_SERVER_TO_CLIENT_SNAPSHOT << eid << tmPacked;
+  ENetPacket *packet = enet_packet_create(nullptr, size, ENET_PACKET_FLAG_UNSEQUENCED);
+  bitstream(packet->data) << E_SERVER_TO_CLIENT_SNAPSHOT << eid << tmPacked;
+
   enet_peer_send(peer, 1, packet);
 }
 
@@ -76,27 +61,21 @@ MessageType get_packet_type(ENetPacket *packet)
   return (MessageType)*packet->data;
 }
 
-void deserialize_new_entity(ENetPacket *packet, Entity &ent)
+ void deserialize_new_entity(ENetPacket *packet, Entity &ent)
 {
-  uint8_t *ptr = packet->data; ptr += sizeof(uint8_t);
-  ent = *(Entity*)(ptr); ptr += sizeof(Entity);
+  bitstream(packet->data) >> Skip<MessageType>{} >> ent;
 }
 
 void deserialize_set_controlled_entity(ENetPacket *packet, uint16_t &eid)
 {
-  uint8_t *ptr = packet->data; ptr += sizeof(uint8_t);
-  eid = *(uint16_t*)(ptr); ptr += sizeof(uint16_t);
+  bitstream(packet->data) >> Skip<MessageType>{} >> eid;
 }
 
 void deserialize_entity_input(ENetPacket *packet, uint16_t &eid, float &thr, float &steer)
 {
-  uint8_t *ptr = packet->data; ptr += sizeof(uint8_t);
-  eid = *(uint16_t*)(ptr); ptr += sizeof(uint16_t);
-  ControlsQuantized ctrlPacked(*(uint8_t*)(ptr)); ptr += sizeof(ControlsQuantized);
-  /*
-  uint8_t thrPacked = *(uint8_t*)(ptr); ptr += sizeof(uint8_t);
-  uint8_t oriPacked = *(uint8_t*)(ptr); ptr += sizeof(uint8_t);
-  */
+  ControlsQuantized ctrlPacked(0);
+  bitstream(packet->data) >> Skip<MessageType>{} >> eid >> ctrlPacked;
+
   static auto neutralPackedValue = pack_float<ControlsQuantized::PackedType>(0.f, -1.f, 1.f, 4);
   Vec2 ctrl = ctrlPacked.unpack({-1., 1.}, {-1., 1.});
 
@@ -106,10 +85,9 @@ void deserialize_entity_input(ENetPacket *packet, uint16_t &eid, float &thr, flo
 
 void deserialize_snapshot(ENetPacket *packet, uint16_t &eid, float &x, float &y, float &ori)
 {
-  uint8_t *ptr = packet->data; ptr += sizeof(uint8_t);
-  eid = *(uint16_t*)(ptr); ptr += sizeof(uint16_t);
-  TransformQuantized tmPacked(*(TransformQuantized *)(ptr)); ptr += sizeof(TransformQuantized);
+  TransformQuantized tmPacked(0);
+  bitstream(packet->data) >> Skip<MessageType>{} >> eid >> tmPacked;
+
   Vec3 tm = tmPacked.unpack({-16., 16.}, {-8., 8.},  {-PI, PI});
   x = tm.x; y = tm.y; ori = tm.z;
 }
-
